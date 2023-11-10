ca6fda7fc7108ec76078d0daffe9a966d51a995b
andigagaga <andigagaga1@gmail.com>
2023-11-10 18:23:00 +0700

    Content-Modal

---
+++ src/navigation/SettingHome.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function SettingHome() {
+    return (
+        <View style={{backgroundColor: 'gray', flex: 1}}>
+            <Text>Ini </Text>
+            <View style={{width: 100, height: 100, backgroundColor: 'blue',  marginTop: 100}}></View>
+        </View>
+    );
+}
